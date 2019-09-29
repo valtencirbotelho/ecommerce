61c72fb431d9d93bf03d23881d5c0f9b8e8beb13
valtencirbotelho <valtencirbotelho1970@gmail.com>
2019-09-29 18:55:39 -0300

    Tela de Login

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use Hcode\Page;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -11,10 +13,48 @@ $app->config('debug', true);
 
 $app->get('/', function() {
 
-$page = new Page();
+	$page = new Page();
 
-$page -> setTpl("index");	
+	$page->setTpl("index");	
 
+});
+
+$app->config('debug', true);
+
+$app->get('/admin', function() {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");	
+
+});
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+	 "header"=>false,
+	 "footer"=>false
+]);
+
+$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST['password']);
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
